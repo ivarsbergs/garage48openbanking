82af213bfa49fc74be8e1d8bde6e5e8022f704e1
Ivars Bergs <ivarsbergs@Ivarss-MacBook-Pro.local>
2018-02-17 14:27:16 +0200

    Add loader view

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -1,46 +1,57 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+//endpoint's
+//http://kidsbank.herokuapp.com/balance/owner
+
+
 var app = {
-    // Application Constructor
-    initialize: function() {
+    bar: null,
+    initialize: function () {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
+    onDeviceReady: function () {
+        var self = this;
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
+        var randomValue = Math.random();
+        this.bar = new ProgressBar.Circle("#progress", {
+            color: '#333333',
+            trailColor: '#EEEEEE',
+            trailWidth: 1,
+            duration: 1400,
+            easing: 'bounce',
+            strokeWidth: 12,
+            from: { color: '#539124', a: 0 },
+            to: { color: '#9EC908', a: 1 },
+            // Set default step function for all animate calls
+            step: function (state, circle) {
+                circle.path.setAttribute('stroke', state.color);
+            },
+            text: {
+                value: (randomValue * 100).toFixed(0) + '%',
+                className: 'progress-bar-label',
+                autoStyle: true
+            }
+        });
+        this.bar.animate(randomValue);  // Number from 0.0 to 1.0
+
+        $(".open-parent-view").click(function () {
+            $(".view").hide();
+            $("#loader-view").show();
+            $.getJSON("http://kidsbank.herokuapp.com/balance/owner", function (data) {
+                $(".view").hide();
+                $("#parent-view").show();
+                var randomValue = Math.random();
+                self.bar.setText((randomValue * 100).toFixed(0) + '%');
+                self.bar.animate(randomValue);
+            });
+        });
+        $(".open-child-view").click(function () {
+            $(".view").hide();
+            $("#child-view").show();
+        });
+        $(".open-parent-child-view").click(function () {
+            $("#parent-child-view").find(".child-name").text($(this).attr("value"));
+            $(".view").hide();
+            $("#parent-child-view").show();
+        });
     },
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
 };
-
 app.initialize();
